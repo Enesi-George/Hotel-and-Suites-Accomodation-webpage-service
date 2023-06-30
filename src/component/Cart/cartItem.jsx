b443f806c97333d25b893cdc3dd8b47e88b3cbec
unknown <georgeenesi81@gmail.com>
2023-06-30 09:07:02 +0100

    modified jsx

--- src/component/Cart/cartItem.jsx
+++ src/component/Cart/cartItem.jsx
@@ -6,7 +6,6 @@ const CartItem = ({ item, onAdd, onRemove }) => {
     return (
       <div className="cart-item">
         <span>{name} </span>
-        {/* <span>{price}</span> */}
         <Button onClick={onAdd} className="px-1 py-0 bg-white text-dark  "> +</Button>
         <span>{quantity}</span>
         <Button onClick={onRemove} className="px-1 py-0 bg-white text-dark "> - </Button>
